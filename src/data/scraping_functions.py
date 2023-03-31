a43fbd024afa5e837d9c0e94b8effb3c5ddf2d36
Sherrif <msherrif04@gmail.com>
2023-03-31 12:27:15 +0000

    scraped spacex launch data from wikipedia

---
+++ src/data/scraping_functions.py
@@ -0,0 +1,54 @@
+import unicodedata
+
+def date_time(table_cells):
+    """
+    This function returns the data and time from the HTML  table cell
+    Input: the  element of a table data cell extracts extra row
+    """
+    return [data_time.strip() for data_time in list(table_cells.strings)][0:2]
+
+def booster_version(table_cells):
+    """
+    This function returns the booster version from the HTML  table cell 
+    Input: the  element of a table data cell extracts extra row
+    """
+    out=''.join([booster_version for i,booster_version in enumerate( table_cells.strings) if i%2==0][0:-1])
+    return out
+
+def landing_status(table_cells):
+    """
+    This function returns the landing status from the HTML table cell 
+    Input: the  element of a table data cell extracts extra row
+    """
+    out=[i for i in table_cells.strings][0]
+    return out
+
+
+def get_mass(table_cells):
+    mass=unicodedata.normalize("NFKD", table_cells.text).strip()
+    if mass:
+        mass.find("kg")
+        new_mass=mass[0:mass.find("kg")+2]
+    else:
+        new_mass=0
+    return new_mass
+
+
+def extract_column_from_header(row):
+    """
+    This function returns the landing status from the HTML table cell 
+    Input: the  element of a table data cell extracts extra row
+    """
+    if (row.br):
+        row.br.extract()
+    if row.a:
+        row.a.extract()
+    if row.sup:
+        row.sup.extract()
+        
+    colunm_name = ' '.join(row.contents)
+    
+    # Filter the digit and empty names
+    if not(colunm_name.strip().isdigit()):
+        colunm_name = colunm_name.strip()
+        return colunm_name    
